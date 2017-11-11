5b2f7c73f65c4883feaad43fb5ed852fc8b47795
AlexanderReyer <33474608+AlexanderReyer@users.noreply.github.com>
2017-11-11 14:42:13 +0100

    Add files via upload
    befehle mit form.get, main mit value statt placeholder

--- homeautomationaccess/telnet.py
+++ homeautomationaccess/telnet.py
@@ -72,11 +72,12 @@ msg = \
 
 
 # Yeelight Port-mapping
-#host = "192.168.178.25"
-#port = 55443
-host = "marssonde.ddns.net"
-port = 80
-
+#host       = "192.168.178.25"
+#port       = 55443
+host        = "marssonde.ddns.net"  # connect to device via port mapping
+hostssdp    = '239.255.255.250'     # search for uPnP device
+port        = 80
+portssdp    = 1982                  # yeelight ssdp-port instead of 1900
 
 '''
 1. action    -> licht
@@ -129,6 +130,7 @@ port = 80
 app = Flask(__name__)
 bootstrap = Bootstrap(app) # Webdesign-features
 
+# -------- webhook for dialogflow ----------
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -155,50 +157,76 @@ def processRequest(req):
     res = "action"
     return res
 
+# -------- INDEX and main template ----------
+# JSON objects will be sent to device via TCP
+#
+# { "id":       1,                         int
+#   "method":   "set_power",               str
+#   "params":   ["on", "smooth", 500]      array
+# }
+#
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    power = 0
-    rgb = 0
-    set_power = ""
-    set_rgb = ""
-    toggle = ""
-    response = ""
+    # values containing device states:
+    power       = 0
+    rgb         = 0
+    # following button-states contain str "pressed" or None
+    get_prop    = request.form.get("get_prop")
+    set_power   = request.form.get("set_power")
+    set_rgb     = request.form.get("set_rgb")
+    toggle      = request.form.get("toggle")
+    response    = ""   # contains the return str sent back by the device
         
     if request.method == "POST":
         print(request.form)
-        power = request.form["powertext"]
-        rgb = request.form["rgbtext"]
-        if request.form.get("set_power"):
-            set_power = request.form["set_power"]
-        if request.form.get("set_rgb"):
-            set_rgb = request.form["set_rgb"]
-        if request.form.get("toggle"):
-            toogle = "1"
+        # get form values that shall change the device state
+        power = request.form.get("powertext")
+        rgb = request.form.get("rgbtext")
 
+    print("Buttons+Texts:", set_power, power, set_rgb, rgb, toggle)
+    #return "debug" # -- Debug --
+
+
+    # get_prop -----------------------------
+    # Usage: This method is used to retrieve current property of smart LED.
+    # Parameters:              1 to N.
+    # The parameter is a list of property names and the response contains a list of corresponding property values.
+    # If the requested property name is not recognized by smart LED, then a empty string value ("") will be returned.
+    # Request Example:     {"id":1,"method":"get_prop","params":["power", "not_exist", "bright"]}
+    # Response Example:  {"id":1, "result":["on", "", "100"]}
+    # NOTE:                          All the supported properties are defined in table 4-2, section 4.3
+    if get_prop is not None:
+        print("yeelight power: ", power, " rgb: ", rgb)
+        print(get_prop)
+
+   
     # set_power -----------------------------
-    if set_power != "":
-        if "0" != request.form["powertext"]:
+    if set_power is not None: #!= "":
+        if "0" != str(power): #request.form["powertext"]:
             params = '["on", "smooth", 500]'
         else:
             params = '["off", "smooth", 500]'
         response = telnettodevice('{ "id": 1, "method": "set_power", "params":' + params + '} \r\n')
         
-    if set_rgb != "": # decimal integer ranges from 0 to 16777215 (hex: 0xFFFFFF). 
+    # set_rgb ------------------------------
+    if set_rgb is not None: # decimal integer ranges from 0 to 16777215 (hex: 0xFFFFFF). 
         params = request.form["rgbtext"]
         response = telnettodevice('{"id":1,"method":"set_rgb","params":[' + params + ', "smooth", 500]} \r\n')
         
-    if toggle != "":
+    # toggle -----------------------------
+    if toggle is not None:
         response = telnettodevice('{"id":1,"method":"toggle","params":[]} \r\n')
 
-    #print("Button Wert set_power: ", set_power)
     return render_template("main.html", power = power, test = set_power, rgb = rgb, response = response)
 
-
+# ----------------------------------------
+# -------- telnet TCP to device ----------
 def telnettodevice(writetext = ""):
     print("oeffne mit telnetlib ", host, ":", port)
     telnet = telnetlib.Telnet()
     telnet.open(host, port, 3)
     #telnet.write('{ "id": 1, "method": "set_power", "params":["on", "smooth", 500]} \r\n'.encode())
+    print(writetext.encode())
     telnet.write(writetext.encode())
     #out = telnet.read_all()
     response = telnet.read_until("\r\n".encode(), 1)
@@ -207,28 +235,48 @@ def telnettodevice(writetext = ""):
     return response
 
 
+# -------- turn on yeelight lamp ----------
 @app.route('/on', methods=['GET', 'POST'])
 def sourceon():
-    print("oeffne mit telnetlib ", host, ":", port)
-    telnet = telnetlib.Telnet()
-    telnet.open(host, port, 3)
-    telnet.write('{ "id": 1, "method": "set_power", "params":["on", "smooth", 500]} \r\n'.encode())
-    #out = telnet.read_all()
-    response = telnet.read_until("\r\n".encode(), 1)
-    print(response)
-    telnet.close()
+    response = telnettodevice('{ "id": 1, "method": "set_power", "params":["on", "smooth", 500]} \r\n')
     return render_template("main.html", response = response)
-    #return response
 
+# -------- ssdp yeelight lamp LAN ---------
+# use ssdp-ip in lan to search for uPnP device
 @app.route('/ssdp', methods=['GET', 'POST'])
-def ssdp():
+def ssdp_lan():
     #
     # https://www.electricmonk.nl/log/2016/07/05/exploring-upnp-with-python/
     #
     # Set up UDP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     s.settimeout(1)
-    s.sendto(msg.encode(), ('239.255.255.250', 1982) )
+    s.sendto(msg.encode(), (hostssdp, portssdp) )
+
+    ''' Response:
+    addr = ('192.168.178.25', 49156) b'
+
+    HTTP/1.1 200 OK\r\n
+    '''
+    try:
+        while True:
+            data, addr = s.recvfrom(65507)
+            print ("addr: ", addr, "\r\ndata ", data)
+    except socket.timeout:
+        pass
+    return render_template("main.html", response = data)
+
+# -------- ssdp yeelight lamp GLOBAL -------
+# use port mapping from fritzbox to connect to ssdp-IP-Addr
+@app.route('/ssdpglobal', methods=['GET', 'POST'])
+def ssdpglobal():
+    #
+    # https://www.electricmonk.nl/log/2016/07/05/exploring-upnp-with-python/
+    #
+    # Set up UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    s.settimeout(1)
+    s.sendto(msg.encode(), (host, portssdp) )
 
     try:
         while True:
@@ -237,7 +285,6 @@ def ssdp():
     except socket.timeout:
         pass
     ''' Response:
-    addr = ('192.168.178.25', 49156) b'
 
     HTTP/1.1 200 OK\r\n
     Cache-Control: max-age=3584\r\n
@@ -274,25 +321,16 @@ def ssdp():
     return render_template("main.html", response = response)
     #return make_response(str(data).encode() + "<br>".encode() + str(addr).encode())
 
+# -------- turn off yeelight lamp ----------
 @app.route('/off', methods=['GET', 'POST'])
 def sourceoff():
-    print("oeffne mit telnetlib ", host, ":", port)
-    telnet = telnetlib.Telnet()
-    telnet.open(host, port, 3)
-    telnet.write('{ "id": 1, "method": "set_power", "params":["off", "smooth", 500]} \r\n'.encode())
-    #out = telnet.read_until(b"DB>", 5)
-    #out = telnet.read_all()
-    response = telnet.read_until("\r\n".encode(), 1)
-    print(response)
-    #telnet.write(b'quit\r\n')
-    print("schliesse Verbindung")
-    telnet.close()
+    response = telnettodevice('{ "id": 1, "method": "set_power", "params":["off", "smooth", 500]} \r\n')
     return render_template("main.html", response = response)
-    #return out
 
+# -------- MAIN LOCALHOST SERVER  ----------
 if __name__ == '__main__':
     serverport = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % serverport)
 
-    app.run(debug=True, port=serverport, host='0.0.0.0')
+    app.run(debug=True, port=serverport) #, host='0.0.0.0')
